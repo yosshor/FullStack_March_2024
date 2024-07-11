352fe8a2d2d6658b9bbdbe402088550df59ef65a
ShorY <yosishor28@gmail.com>
2024-07-11 15:09:40 +0300

    show select item per user

--- 04 - TypeScript/17 - MVC-2 - 08-07-2024/homePuzzles/Yos/yos-project-example/src/controllers/showItems.ts
+++ 04 - TypeScript/17 - MVC-2 - 08-07-2024/homePuzzles/Yos/yos-project-example/src/controllers/showItems.ts
@@ -4,20 +4,36 @@ import renderItem from "../views/items/items";
 
 export var currentUser: User;
 
+function showSelectedItems(): string | null {
+    try {
+        const itemsSelected = currentUser.cart.map(id => items.find(i => i.id === id)!.name)
+        console.log(itemsSelected)
+        let select = `<select class="show-selected-items" name="show-items" id="show-item-select"> 
+                    ${itemsSelected.length > 0 ?
+                         itemsSelected.map(item => `<option value=${item}>${item}</option>`).join('')  :
+                         "<option value=''>No Data Found</option>"}
+                    </select>`
+        console.log(select)
+        return select
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+}
+
 export function showItems(user: User): string {
     try {
         currentUser = user;
-        // console.log('the user is', user)
         let container = document.getElementById('app') as HTMLDivElement;
         const allItems = `<div class='wrapper'>
                             <div class='show-user'>
                             <p>User Image : </p> <img src=${user.img} alt=${user.name}>
                             <p>User Name Is : ${user.name}</p>
                             <p>Items In Cart : ${user.cart.length}</p>
+                            ${showSelectedItems() !== null ? showSelectedItems() : ''}
                         </div><div class=container>
                         ${items.map(i => renderItem(i)).join('')}
                         ${items.map(i => renderItem(i)).join('')}</div></div>`
-        // console.log(container);
         container.innerHTML = allItems;
         return allItems;
     } catch (error) {
@@ -43,9 +59,9 @@ function handleRemoveItem(id: string): void {
         console.log(id)
         const item = currentUser.cart.indexOf(id);
         console.log(item)
-        if(item >= 0){
-                currentUser.cart.splice(item, 1);
-                showItems(currentUser);
+        if (item >= 0) {
+            currentUser.cart.splice(item, 1);
+            showItems(currentUser);
         }
     } catch (error) {
         console.error(error);
