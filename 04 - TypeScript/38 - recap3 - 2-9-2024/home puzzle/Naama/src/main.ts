01979d3f7e9148354f267827f78965f7f37c7929
ShorY <yosishor28@gmail.com>
2024-09-24 14:55:33 +0300

    delete home puzzle folder

--- 04 - TypeScript/38 - recap3 - 2-9-2024/home puzzle/Naama/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { heros } from "./model/superHero";
-import { renderHeros } from "./view/renderHeroes";
-import './style.scss';
-import { renderAddBtn } from "./view/renderAdding";
-
-renderAddBtn();
-renderHeros(heros);
