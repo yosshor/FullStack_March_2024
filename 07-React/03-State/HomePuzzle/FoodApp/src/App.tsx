38d0662c43687cc02168508a676be80d4eb48c55
ShorY <yosishor28@gmail.com>
2024-11-20 16:34:45 +0200

    change the page with buttons

--- 07-React/03-State/HomePuzzle/FoodApp/src/App.tsx
+++ 07-React/03-State/HomePuzzle/FoodApp/src/App.tsx
@@ -1,15 +1,21 @@
-import { useState } from 'react'
-import './App.css'
-import Food from './view/components/FoodApi/Food'
-import GithubUserProfile from './view/components/GithubUserProfile/GithubUserProfile'
+import { useState } from 'react';
+import './App.css';
+import Food from './view/components/FoodApi/Food';
+import GithubUserProfile from './view/components/GithubUserProfile/GithubUserProfile';
 
 function App() {
+  const [view, setView] = useState('github');
+
   return (
-    <>
-      <GithubUserProfile />
-      <Food />
-    </>
-  )
+    <div>
+      <div>
+        <button onClick={() => setView('github')}>Github User Profile</button>
+        <button onClick={() => setView('food')}>Food</button>
+      </div>
+      {view === 'github' && <GithubUserProfile />}
+      {view === 'food' && <Food />}
+    </div>
+  );
 }
 
-export default App
+export default App;
