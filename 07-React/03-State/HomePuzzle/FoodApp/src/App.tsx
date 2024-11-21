f1f84baa5b77dbc3649e7e78be0a80128c5c3a85
ShorY <yosishor28@gmail.com>
2024-11-21 13:31:22 +0200

    add style to home buttons

--- 07-React/03-State/HomePuzzle/FoodApp/src/App.tsx
+++ 07-React/03-State/HomePuzzle/FoodApp/src/App.tsx
@@ -10,9 +10,9 @@ function App() {
   return (
     <div>
       <div>
-        <button onClick={() => setView('github')}>Github User Profile</button>
-        <button onClick={() => setView('food')}>Food</button>
-        <button onClick={() => setView('generateImages')}>Generate Images</button>
+        <button className='home-buttons' onClick={() => setView('github')}>Github User Profile</button>
+        <button className='home-buttons' onClick={() => setView('food')}>Food</button>
+        <button className='home-buttons' onClick={() => setView('generateImages')}>Generate Images</button>
       </div>
       {view === 'github' && <GithubUserProfile />}
       {view === 'food' && <Food />}
